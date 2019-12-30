cf57f5dcf8db232eeba280ee9f7a0ac1135259fc
aleksej <aleksej.paschenko@gmail.com>
2019-12-30 15:44:00 +0300

    replace harcoded ip of minikube cluster to $(minikube ip)

--- connect-minikube-cluster.sh
+++ connect-minikube-cluster.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 ./shipa pool-add kube --provisioner=kubernetes
-addr="192.168.99.104:8443"
+addr="$(minikube ip):8443"
 
-MINIKUBE_CA="/home/aleksej/.minikube/ca.crt"
+MINIKUBE_CA="$HOME/.minikube/ca.crt"
 
-CLIENTCERT="/home/aleksej/.minikube/client.crt"
-CLIENTKEY="/home/aleksej/.minikube/client.key"
-
+CLIENTCERT="$HOME/.minikube/client.crt"
+CLIENTKEY="$HOME/.minikube/client.key"
 
 ./shipa cluster-add kube-cluster kubernetes --pool kube --addr $addr --cacert=$MINIKUBE_CA --clientkey=$CLIENTKEY --clientcert=$CLIENTCERT
